f2d34f0b9f4dd880ee6fb2c13ba31cf28514df9e
Richard Cho <richardcho100@gmail.com>
2018-10-11 01:08:19 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,26 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "Black", "Energetic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "Female", "Brown", "Curious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 7, "Male","Black", "Agressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "Female", "Black", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "Male", "Brown", "Lazy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "Female", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4, "Male", "Black", "Agressive", 0);
+
+-- INSERT INTO bears VALUES (1, "Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+-- INSERT INTO bears VALUES (2, "Rowdy", 10, "M", "black", "intense", 1);
+-- INSERT INTO bears VALUES (3, "Tabitha", 6, "F", "dark brown", "Nice", 0);
+-- INSERT INTO bears VALUES (4, "Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+-- INSERT INTO bears VALUES (5, "Melissa", 13, "F", "dark brown", "goofy", 1);
+-- INSERT INTO bears VALUES (6, "Grinch", 2, "M", "Black", "Grinchy", 1);
+-- INSERT INTO bears VALUES (7, "Wendy", 12, "F", "Blue", "naive", 0);
+-- INSERT INTO bears VALUES (8, null, 20, "M", "black", "aggressive", 0);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "dark brown", "Nice", 0);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "F", "dark brown", "goofy", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", "Black", "Grinchy", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "F", "Blue", "naive", 0);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 20, "M", "black", "aggressive", 0);
